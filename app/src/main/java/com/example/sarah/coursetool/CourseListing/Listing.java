0d0b7134aba68c097e8f5afd251b53aff517644d
L Toal <lc790935@dal.ca>
2018-06-15 11:49:25 -0300

    LT & NS Working on adding view for courses 2

---
+++ app/src/main/java/com/example/sarah/coursetool/CourseListing/Listing.java
@@ -0,0 +1,13 @@
+package com.example.sarah.coursetool.CourseListing;
+
+public class Listing {
+    public String title;
+    public String description;
+    public int imageId;
+
+    public Listing(String title, String description, int imageId) {
+        this.title = title;
+        this.description = description;
+        this.imageId = imageId;
+    }
+}
