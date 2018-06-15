5789f231b20f53691644d5a73c2dbe7326c5c2e8
noah-sealy <noahsealy@hotmail.com>
2018-06-15 18:07:53 -0300

    - LT & NS have made the Listings.java class ready for the new implementation of the viewAdapter.java (which will now be designed for the CourseListing data object).

--- app/src/main/java/com/example/sarah/coursetool/CourseListing/Listings.java
+++ app/src/main/java/com/example/sarah/coursetool/CourseListing/Listings.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.example.sarah.coursetool.R;
 
@@ -18,21 +19,50 @@ public class Listings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listings);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
-        ArrayList<Listing> inputData = new ArrayList<Listing>();
-            Listing l1 = new Listing("Halifax", "Nova Scotia", 1);
-            Listing l2 = new Listing("Moncton", "New Brunswick", 2 );
-            Listing l3 = new Listing("Toronto", "Ontario", 3);
-            Listing l4 = new Listing("Montreal", "Quebec", 4);
-            Listing l5 = new Listing("Vancouver", "British Columbia", 5);
+                Date start1 = new Date(1967, 03, 04);
+                Date end1 = new Date(1970, 03, 05);
+                Date start2 = new Date(2017, 04, 06);
+                Date end2 = new Date(2017, 12, 07);
+                Date start3 = new Date(1960, 03, 02);
+                Date end3 = new Date(1970, 05, 03);
+                Date start4 = new Date(1980, 01, 01);
+                Date end4 = new Date(1990, 01, 01);
+                Date start5 = new Date(1900, 04, 02);
+                Date end5 = new Date(1997, 05, 03);
 
-            inputData.add(l1);
-            inputData.add(l2);
-            inputData.add(l3);
-            inputData.add(l4);
-            inputData.add(l5);
+                ArrayList<CourseListing> inputData = new ArrayList<CourseListing>();
+
+                CourseListing course1 = new CourseListing("Introduction to the Tyrannosaurus Rex",
+                                                            "Dr Fossils", "Archaeology",
+                                                            "Not for the faint of heart", start1, end1,
+                                                            275638, 4);
+
+                CourseListing course2 = new CourseListing("All About Rocks",
+                                                            "Dr Stone", "Archaeology",
+                                                            "Learning rocks!", start2, end2,
+                                                            14506, 9);
+
+                CourseListing course3 = new CourseListing("Advanced Animal Behaviour",
+                                                            "Dr Moose", "Psychology",
+                                                            "Cats, dogs, and more!", start3, end3,
+                                                            18013, 6);
+
+                CourseListing course4 = new CourseListing("Introduction to Data Structures",
+                                                            "Dr Bitwise", "Computer Science",
+                                                            "course4.desc = \"Fun course!\"", start4, end4,
+                                                            34829, 7);
+
+                CourseListing course5 = new CourseListing("How to Succeed in Business",
+                                                            "Dr Moneybags", "Business",
+                                                            "Learn how to make money from others for only $1350!", start4, end4,
+                                                            40392, 12);
+
+                inputData.add(course1);
+                inputData.add(course2);
+                inputData.add(course3);
+                inputData.add(course4);
+                inputData.add(course5);
 
         RecyclerView courseListContainer = (RecyclerView) findViewById(R.id.recycleView);
         courseListContainer.setHasFixedSize(true);
