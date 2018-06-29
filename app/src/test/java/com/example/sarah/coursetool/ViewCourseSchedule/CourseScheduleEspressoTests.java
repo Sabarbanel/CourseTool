40be85be7ce11e0a33dc5b471ec713d32b35e49d
sarah abarbanel <scabarbanel@gmail.com>
2018-06-29 11:44:31 -0300

    CT-7-L&S Adding test files, adding buttons to weekview

---
+++ app/src/test/java/com/example/sarah/coursetool/ViewCourseSchedule/CourseScheduleEspressoTests.java
@@ -0,0 +1,12 @@
+package com.example.sarah.coursetool.ViewCourseSchedule;
+
+
+public class CourseScheduleEspressoTests {
+    // test button is clickable onView(withId(R.id.your_button)).check(matches(isClickable()));  (https://stackoverflow.com/questions/32906881/checking-if-a-button-is-clickable-in-espresso-test-android-studio)
+    //click button
+    //after clicking, check that monday exists, if it doesn't, the button click failed
+
+    //repeat for dropdown and main menu
+    //repeat for going from week to day. Query on a specific timeslot
+
+}
---
+++ app/src/test/java/com/example/sarah/coursetool/ViewCourseSchedule/CourseScheduleUnitTests.java
@@ -0,0 +1,5 @@
+package com.example.sarah.coursetool.ViewCourseSchedule;
+
+public class CourseScheduleUnitTests {
+    //
+}
