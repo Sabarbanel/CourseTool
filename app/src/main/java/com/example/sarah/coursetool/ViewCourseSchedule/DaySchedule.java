1bf4a750bc94b20a88fb898465ac06cad4f2592a
L Toal <lc790935@dal.ca>
2018-07-04 15:15:36 -0300

    LT & SA: Added a day schedule view and java class.

--- app/src/main/java/com/example/sarah/coursetool/ViewCourseSchedule/DaySchedule.java
+++ app/src/main/java/com/example/sarah/coursetool/ViewCourseSchedule/DaySchedule.java
@@ -1,18 +1,14 @@
 package com.example.sarah.coursetool.ViewCourseSchedule;
 
-import android.os.Bundle;
-
 import com.example.sarah.coursetool.BaseNavigationActivity;
 import com.example.sarah.coursetool.R;
+import android.os.Bundle;
 
-public class WeekSchedule extends BaseNavigationActivity {
+public class DaySchedule extends BaseNavigationActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_week_schedule);
-
-
+        setContentView(R.layout.activity_day_schedule);
     }
-
 }
