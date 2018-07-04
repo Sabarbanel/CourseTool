865c1303beffb90fe00b3656a3435eaddc34eeae
L Toal <lc790935@dal.ca>
2018-07-04 15:00:44 -0300

    LT & SA: Renamed day to week for weekview.

--- app/src/main/java/com/example/sarah/coursetool/ViewCourseSchedule/WeekSchedule.java
+++ app/src/main/java/com/example/sarah/coursetool/ViewCourseSchedule/WeekSchedule.java
@@ -1,11 +1,6 @@
 package com.example.sarah.coursetool.ViewCourseSchedule;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.sarah.coursetool.BaseNavigationActivity;
 import com.example.sarah.coursetool.R;
@@ -15,7 +10,7 @@ public class DaySchedule extends BaseNavigationActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_day_schedule);
+        setContentView(R.layout.activity_week_schedule);
 
 
     }
