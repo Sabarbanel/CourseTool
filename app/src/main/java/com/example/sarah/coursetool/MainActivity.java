c0ce25147f49a10c28adc43eeabfecaaba051782
L Toal <lc790935@dal.ca>
2018-07-10 12:56:44 -0300

    SA & LT: Adjusted main menu so that clicking View Schedule now directs to the schedule page.

--- app/src/main/java/com/example/sarah/coursetool/MainActivity.java
+++ app/src/main/java/com/example/sarah/coursetool/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.sarah.coursetool;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.sarah.coursetool.ViewCourseSchedule.WeekSchedule;
 
 public class MainActivity extends BaseNavigationActivity {
 
@@ -8,5 +16,22 @@ public class MainActivity extends BaseNavigationActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listview = findViewById(R.id.main_nav_option_list);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?>adapter,View v, int position, long a){
+                String item = (String) adapter.getItemAtPosition(position);
+                if(item.equals("View Schedule")) {
+                    Intent intent = new Intent(MainActivity.this, WeekSchedule.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    public void goToSchedule(View v) {
+        Intent intent = new Intent(this, WeekSchedule.class);
+        startActivity(intent);
     }
 }
