c1a578f1570e00e24bb4f5c79b24deffe7f2b2cf
Hamza <hamza.awan@dal.ca>
2018-07-11 13:16:40 -0300

    "CT-8-enroll, HA&NO, made changes to register activity by adding functionality for Add Course button"

--- app/src/main/java/com/example/sarah/coursetool/RegisterActivity.java
+++ app/src/main/java/com/example/sarah/coursetool/RegisterActivity.java
@@ -1,12 +1,62 @@
 package com.example.sarah.coursetool;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sarah.coursetool.Course.CourseInterface;
+import com.example.sarah.coursetool.Database.LoginDatabase;
+import com.example.sarah.coursetool.Database.StudentDatabase;
+import com.example.sarah.coursetool.Database.UserDatabase;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
 public class RegisterActivity extends BaseNavigationActivity {
 
+    StudentDatabase studentDatabase;
+    LoginDatabase loginDatabase;
+    UserDatabase userDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        loginDatabase = new LoginDatabase(this);
+        //get the user database for the user currently logged in
+        userDatabase = loginDatabase.getProfileDatabase("rlowe90", "lordOfTheRings340");
+    }
+    /*
+    Called when 'Add Course' button is pressed
+     */
+    public void addCourse(View v){
+        Button addButton = findViewById(R.id.addCourse);
+        EditText codeField = findViewById(R.id.courseCode);
+        String c = codeField.getText().toString();
+        //convert the user entry into an int
+        int code = Integer.parseInt(c);
+        //check if the course entered by the user is in their list of courses already by ID
+        ArrayList<CourseInterface>courses = userDatabase.getScheduledCourses();
+        CourseInterface scheduledCourse = null;
+        for(CourseInterface course : courses){
+            if(course.getID() == code){
+                scheduledCourse = course;
+            }
+            else{
+                scheduledCourse = null;
+            }
+        }
+        //check to see if text field contains content
+        if(courses.contains(scheduledCourse)){
+            try{
+                userDatabase.enroll(code);
+                Toast toast = Toast.makeText(getApplicationContext(), "Course added successfully!", Toast.LENGTH_SHORT);
+            }
+            catch(InvalidParameterException ipe){
+                Toast toast = Toast.makeText(getApplicationContext(), ipe.getMessage(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
     }
 }
