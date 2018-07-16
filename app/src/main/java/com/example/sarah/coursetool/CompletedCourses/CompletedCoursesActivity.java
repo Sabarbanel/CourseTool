b85d63b7563e3b114d97f3e1773979b1be0c4c9f
Noah Attwood <noah.attwood@dal.ca>
2018-07-16 18:50:51 -0300

    NA&LT, created inital Completed Courses activity

---
+++ app/src/main/java/com/example/sarah/coursetool/CompletedCourses/CompletedCoursesActivity.java
@@ -0,0 +1,15 @@
+package com.example.sarah.coursetool.CompletedCourses;
+
+import android.os.Bundle;
+
+import com.example.sarah.coursetool.BaseNavigationActivity;
+import com.example.sarah.coursetool.R;
+
+public class CompletedCoursesActivity extends BaseNavigationActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_courses);
+    }
+}
